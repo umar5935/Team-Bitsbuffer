1ec1a5f2ffc0535a828e30b8c745ab0d6d5473e1
umar5935 <umarhayyatbbc@gmail.com>
2024-03-09 12:50:26 +0500

    form validations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { FormComponent } from './form/form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "headercomponent" , component: HeaderComponent},
+    {path: "form" , component: FormComponent},
+    {path: "gallery" , component: GalleryComponent},
+
+
+];
